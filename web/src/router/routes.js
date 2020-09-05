46350723f5eadb7a1feeaf1c87c247d5c5aa4a9d
GinBe <34943267+GinBe@users.noreply.github.com>
2020-09-05 21:19:30 +0800

    Q

--- web/src/router/routes.js
+++ web/src/router/routes.js
@@ -1,44 +1,50 @@
-const routes = [{
-  path: '/',
-  component: () => import('layouts/MainLayout.vue'),
-  children: [
-    // { path: '', redirect: 'name' },
-    {
-      name: 'name',
-      path: 'login',
-      component: () => import('pages/Login.vue'),
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      // { path: '', redirect: 'name' },
+      {
+        name: 'name',
+        path: 'login',
+        component: () => import('pages/Login.vue'),
+      },
+      {
+        path: 'game_player',
+        component: () => import('pages/game_player.vue'),
+      },
+    ],
+  },
+  {
+    path: '/manager',
+    component: () => import('layouts/SubLayout.vue'),
+    children: [{
+      path: '',
+      redirect: 'game',
     },
-    {
-      path: 'game_player',
-      component: () => import('pages/game_player.vue'),
-    },
-  ],
-},
-{
-  path: '/manager',
-  component: () => import('layouts/SubLayout.vue'),
-  children: [{
-    path: '',
-    redirect: 'game',
+      {
+        path: 'user',
+        component: () => import('pages/User.vue'),
+      },
+      {
+        path: 'game',
+        component: () => import('pages/Games.vue'),
+      },
+      {
+        path: 'game_host',
+        component: () => import('pages/Game_host.vue'),
+      },
+      //game_host_copy
+      {
+        path: 'game_host_copy',
+        component: () => import('pages/Game_host_copy.vue'),
+      },
+      {
+        path: 'hospital',
+        component: () => import('pages/Hospital.vue'),
+      },
+    ],
   },
-  {
-    path: 'user',
-    component: () => import('pages/User.vue'),
-  },
-  {
-    path: 'game',
-    component: () => import('pages/Games.vue'),
-  },
-  {
-    path: 'game_host',
-    component: () => import('pages/Game_host.vue'),
-  },
-  {
-    path: 'hospital',
-    component: () => import('pages/Hospital.vue'),
-  },
-  ],
-},
 ];
 
 // Always leave this as last one
